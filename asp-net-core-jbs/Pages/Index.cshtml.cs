1a450ff28108165ed120187dfce9f70e3ab23f66
Jhonatan <jhonatanbritosilva@outlook.com>
2023-12-10 07:46:25 -0300

    Update

--- asp-net-core-jbs/Pages/Index.cshtml.cs
+++ asp-net-core-jbs/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace asp_net_core_jbs.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel ( ILogger<IndexModel> logger )
+        public IndexModel ( ILogger<IndexModel> logger, IConfiguration configuration   I)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet ()
         {
-
+            ViewData["Greetings"] = _configuration["Greetings"];
         }
     }
 }
